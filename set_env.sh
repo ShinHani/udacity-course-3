238ef6006cc95f718af0f4813e7ab18dde8a6b08
shincn9120 <hungnq9120@gmail.com>
2023-10-04 01:51:26 +0700

    connect to RDS

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
+export POSTGRES_USERNAME=hungnq45
 export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=hani.cqsuvt5mjjkd.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=my-aws-bucket-03102023
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
