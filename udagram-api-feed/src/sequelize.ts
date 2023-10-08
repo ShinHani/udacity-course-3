84928e4501886c5a130ea2d188a29ebf28fa31ce
shincn9120 <hungnq9120@gmail.com>
2023-10-08 18:03:58 +0700

    config docker

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,5 +1,5 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 
 export const sequelize = new Sequelize({
@@ -8,6 +8,6 @@ export const sequelize = new Sequelize({
   'database': config.database,
   'host': config.host,
 
-  'dialect': config.dialect,
+  'dialect': 'postgres',
   'storage': ':memory:',
 });
