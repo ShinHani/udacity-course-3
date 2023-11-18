2f4f0afaf90edb5f1ec9460485fea3391e92b2f8
shincn9120 <hungnq9120@gmail.com>
2023-11-19 06:38:42 +0700

    update port

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://adee8a9f55eed4621a74578a16b4a95a-1231364621.us-east-1.elb.amazonaws.com/api/v0'
+  apiHost: 'http://af2069bbdc77e4d7db56aded4492fa93-12525745.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
