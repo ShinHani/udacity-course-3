d51a00f51b51ea1849f1bf39f5c971897ca47cf5
shincn9120 <hungnq9120@gmail.com>
2023-11-18 12:21:16 +0700

    update continuous-integration

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=hungnq45
 export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=hani.cqsuvt5mjjkd.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=hani.caxxiyrgrv1z.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=my-aws-bucket-03102023
+export AWS_BUCKET=my-aws-bucket-06112023-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
