edb80bd3a30dcb8be7615635b979f7ce4adb6d3c
shincn9120 <hungnq9120@gmail.com>
2023-11-19 22:23:18 +0700

    rebuild docker

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://af2069bbdc77e4d7db56aded4492fa93-12525745.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
